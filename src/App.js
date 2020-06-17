0ae8349921a5e8276f4fef94909c1a5ae6c24e49
FaresMenzli <fares.elouissi@esprit.tn>
2020-06-17 16:38:18 +0100

    all data

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import UserList from './UserList'
+
+const App = () => {
+  return (
+    <div>
+      <header>Getting used to APIs
+</header>
+      <UserList></UserList>
+    </div>
+  )
+}
+
+export default App
+
